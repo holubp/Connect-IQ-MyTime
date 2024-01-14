7963bcfa7cdf447f4292c5025c3573922fc53a46
Petr Holub <hopet@ics.muni.cz>
2024-01-14 21:55:32 +0100

    reimplementing moon phases using timestamps

---
+++ full-moon/get-full-moons-unix.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+import datetime
+import ephem
+from typing import List, Tuple
+
+# https://stackoverflow.com/questions/2526815/moon-lunar-phase-algorithm
+
+def get_full_moons_in_year(year):
+    """
+    Generate a list of full moons for a given year calibrated to the local time zone
+    :param year: year to determine the list of full moons
+    :return: list of dates as strings in the format YYYY-mm-dd
+    """
+    moons = []
+
+    date = ephem.Date(datetime.date(year - 1, 12, 31))
+    end_date = ephem.Date(datetime.date(year + 1, 1, 1))
+
+    while date <= end_date:
+        date = ephem.next_full_moon(date)
+
+        # Convert the moon dates to the local time zone, add to list if moon date still falls in desired year
+        local_date = ephem.localtime(date)
+        if local_date.year == year:
+            # Append the date as a string to the list for easier comparison later
+            moons.append(int(local_date.timestamp()))
+
+    return moons
+
+full_moons = []
+for i in range(2023, 2050):
+    full_moons += get_full_moons_in_year(i)
+
+print(full_moons)
